075424cff6da4709ea614fd91d94643b57af133a
Nelson Lee <mushy1693@live.com>
2015-04-13 01:07:26 -0700

    added comment

--- third.py
+++ third.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageTk, ImageOps
 #Image is Pillow, ImageTK is the way to use images within tkinter, ImageOps used to work with the image after resizing
 from math import *
 
+# Initialize the variables
 score1 = 0
 score2 = 0
 pturn = "p1"
@@ -33,10 +34,9 @@ def main():
 
    center_window(618,650)
 
+   # Create the Frame for each part
    playerAndScore = Frame(root)
    playerAndScore.pack()
-   wordPanel = Frame(root)
-   wordPanel.pack()
    drawingframe = Frame(root)
    drawingframe.pack()
    inputPanel = Frame(root)
@@ -44,14 +44,17 @@ def main():
    submitAndNextPanel = Frame(root)
    submitAndNextPanel.pack()
 
+   # Declare canvas
    canvas = Canvas(drawingframe, bg='white', width=610, height=500)
 
+   # Allow players to draw on canvas
    def draw( event ):
       x1, y1 = ( event.x - 1 ), ( event.y - 1 )
       x2, y2 = ( event.x + 1 ), ( event.y + 1 )
       canvas.create_rectangle( x1, y1, x2, y2, fill = "black" )
       #http://www.java2s.com/Tutorial/Python/0360__Tkinker/Canvaspaintprogram.htm
 
+   # Check if user made the right guess
    def check(getCheck):
       if(getCheck == "correct"):
         correctLabel = 'correct'
@@ -66,19 +69,11 @@ def main():
       label1.photo = photo
       label1.pack()
 
+   # Clear the canvas
    def clearCanvas():
       canvas.delete("all")
 
-   def word_label(label):
-      def count():
-        label.config(text= "")
-      count()
-
-   def submit_label(label):
-      def count():
-        label.config(text= "Click next for your turn to draw")
-      count()
-
+   # Change label when Done button is clicked
    def turn_label(label):
       def count():
         global pturn
@@ -88,6 +83,13 @@ def main():
             label.config(text= "Player 1 Guess" + "\n" + "Tip: Click 'Submit' when ready")
       count()
 
+   # Change label when Submit button is clicked
+   def submit_label(label):
+      def count():
+        label.config(text= "Click next for your turn to draw")
+      count()
+
+   # Change score on label when Next button is clicked
    def counter_label(label, score):
       def count():
         if(score == "score1"):
@@ -98,14 +100,16 @@ def main():
             label.config(text= "Player 2 Turn" + "\n" + "Score: " + str(score2))
       count()
 
+   # Stop canvas from drawing and change message
    def halt(label1):
       canvas.bind("<B1-Motion>", lambda e: "break")
-      word_label(label1)
       turn_label(label1)
 
+   # Remove all widget on the Frames
    def removeWidget(widget):
       widget.destroy()
 
+   # Determine the players turn
    def choosePlayer():
        global pturn
        if(pturn == "p1"):
@@ -113,6 +117,7 @@ def main():
        else:
            pturn = "p1"
 
+   # Choose color for players turn
    def chooseColor():
        global pturn
        global p1Color
@@ -124,6 +129,7 @@ def main():
            p1Color = "khaki3"
            p2Color = "SkyBlue2"
 
+   # Define game
    def game():
 
       global score1
@@ -132,6 +138,7 @@ def main():
       global time
       global word
 
+      # Move to next players turn
       def newGame():
 
          choosePlayer()
@@ -149,21 +156,27 @@ def main():
 
       chooseColor()
 
+      # Define player1, player2, and Info Label
       player1label = Label(playerAndScore, text="~Player 1 Turn~" + "\n" + "Score: " + str(score1), width = 16, height = 5, background = p1Color)
       player1label.pack(side = LEFT)
 
-      wordlabel= Label(playerAndScore, text="Word: " + word + "\n" + "Time Left: " + str(time) + "\n" + "Tip: Click 'Done' when finish drawing", width = 53, height = 5, background = "goldenrod4")
+      wordlabel= Label(playerAndScore, text="Word: " + word + "\n" + "Time Left: " + str(time) + "\n" + "Tip: Click 'Done' when finish drawing", width = 54, height = 5, background = "goldenrod4")
       wordlabel.pack(side = LEFT)
 
       player2label = Label(playerAndScore, text="~Player 2 Turn~" + "\n" + "Score: " + str(score2), width = 16, height = 5, background = p2Color)
       player2label.pack(side = LEFT)
 
+      # Bind draw to the canvas
       canvas.pack()
       canvas.bind( "<B1-Motion>", draw )
 
+      # Create a text box field
       inputBox = Entry(inputPanel,width=50)
       inputBox.pack()
 
+      # Calculate score and update score
+      # Check if answer is correct and alert player answer
+      # Change label after submit is clicked
       def submit():
           global getWord
           global score1
@@ -184,21 +197,17 @@ def main():
               check("incorrect")
               submit_label(wordlabel)
 
-
+      # Create Clear, Done, Submit, and Next button
       clear_button=Button(submitAndNextPanel, text="Clear", command=lambda: clearCanvas())
       clear_button.pack(side = LEFT)
-
-      if(pturn == "p1"):
-          done_button=Button(submitAndNextPanel, text="Done", command=lambda: halt(wordlabel))
-      else:
-          done_button=Button(submitAndNextPanel, text="Done", command=lambda: halt(wordlabel))
+      done_button=Button(submitAndNextPanel, text="Done", command=lambda: halt(wordlabel))
       done_button.pack(side = LEFT)
-
       submit_button=Button(submitAndNextPanel, text="Submit", command=lambda: submit())
       submit_button.pack(side = LEFT)
       next_button=Button(submitAndNextPanel, text="Next", command=lambda: newGame())
       next_button.pack(side = LEFT)
 
+   # Start game and run loop
    game()
    root.mainloop()
 
